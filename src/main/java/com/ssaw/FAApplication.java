7d5375f08749f597c82b924f926fe04eeb9b1fba
plum <15120022843@126.com>
2020-08-31 15:50:45 +0800

    FA金融估值核资系统Demo

---
+++ src/main/java/com/ssaw/FAApplication.java
@@ -0,0 +1,11 @@
+package com.ssaw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FAApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FAApplication.class,args);
+    }
+}
