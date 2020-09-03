f9230b80e13301e72120d4cd9da4c5801df83e51
hui <2597554309@qq.com>
2020-09-03 20:13:49 +0800

    曾钦辉2020/09/03

--- src/main/java/com/ssaw/BusinessDescription/controller/FundController.java
+++ src/main/java/com/ssaw/BusinessDescription/controller/FundController.java
@@ -4,6 +4,7 @@ import com.ssaw.BusinessDescription.entity.Fund;
 import com.ssaw.BusinessDescription.service.FundService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import javax.annotation.Resource;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -16,50 +17,49 @@ import java.util.Map;
  * description: 基金信息Controller控制层
  * create time: 2020/9/1 11:14
  *
-  * @Param: null
+ * @Param: null
  * @return
  */
 @RestController
 public class FundController {
     @Resource
     FundService fundService;
+
     @RequestMapping(value = "/insertFund")
-    public int insertFund(Fund fund){
+    public int insertFund(Fund fund) {
         System.out.println("进来了");
-        SimpleDateFormat formatter= new SimpleDateFormat("yyyy-MM-dd  HH:mm:ss ");
-        Date date = new Date(System.currentTimeMillis());
-        String format = formatter.format(date);
-        Fund fund1=new Fund(1,"1",1,"1","1",1,
-                1,1,1,1,format,"1","1");
-        int i= fundService.insertFund(fund1);
-        System.out.println(i);
+
+        int i = fundService.insertFund(fund);
+        System.out.println(fund);
         return i;
     }
+
     @RequestMapping(value = "/deleteFund")
-    public void deleteFund(){
+    public void deleteFund(int fundId) {
         System.out.println("进来了");
-        fundService.deleteFund(1);
+        fundService.deleteFund(fundId);
     }
+
     @RequestMapping(value = "/updateFund")
-    public int updateFund(){
+    public int updateFund(Fund fund) {
         System.out.println("进来了");
-        Fund fund=new Fund(1,"1",1,"1","1",1,
-                1,1,1,1,"format","1","1");
-      int i= fundService.updateFund(fund);
-      return i;
+        int i = fundService.updateFund(fund);
+        System.out.println(fund);
+        return i;
     }
+
     @RequestMapping(value = "/selectFund")
-    public Map<String,Object> selectFund(String page, String limit){
+    public Map<String, Object> selectFund(String page, String limit) {
         System.out.println("进来了");
-        Map<String, Object> map =fundService.selectFund(limit,page);
+        Map<String, Object> map = fundService.selectFund(limit, page);
         List<Fund> fundList = (List<Fund>) map.get("fundList");
         int count = (int) map.get("count");
         //以layui要求存储响应数据格式
         Map<String, Object> json = new HashMap<>();
-        json.put("code",0);
-        json.put("msg","");
-        json.put("count",count);
-        json.put("data",fundList);
+        json.put("code", 0);
+        json.put("msg", "");
+        json.put("count", count);
+        json.put("data", fundList);
         //返回数据
         return json;
     }
