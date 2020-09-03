f2fd7030bd9c3ca8753992f9b713a90bed1071b7
sunH_ <779874728@qq.com>
2020-09-03 20:16:43 +0800

    孙浩2020/09/03

--- src/main/java/com/ssaw/InventoryManagement/controller/CashClosedPayInventoryController.java
+++ src/main/java/com/ssaw/InventoryManagement/controller/CashClosedPayInventoryController.java
@@ -1,5 +1,6 @@
 package com.ssaw.InventoryManagement.controller;
 
+import com.ssaw.BusinessData.entity.EquityData;
 import com.ssaw.InventoryManagement.entity.CashClosedPayInventory;
 import com.ssaw.InventoryManagement.service.CashClosedPayInventoryService;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.annotation.Resource;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @program:现金应收应付库存模块
@@ -15,7 +17,6 @@ import java.util.List;
  * @author:孙浩
  * @create:2020-09-01
  */
-@RequestMapping("CashClosedPayInventory")
 @RestController
 public class CashClosedPayInventoryController {
     @Resource
@@ -23,6 +24,9 @@ public class CashClosedPayInventoryController {
 
     @RequestMapping("insertCashClosedPayInventory")
     public int insertCashClosedPayInventory(CashClosedPayInventory cashClosedPayInventory){
+        cashClosedPayInventory.setCrcsId("20200903");
+        cashClosedPayInventory.setFundId("289289289");
+        cashClosedPayInventory.setFundName("一度教育基金");
         int j = cashClosedPayInventoryService.insertCashClosedPayInventory(cashClosedPayInventory);
         return j;
     }
@@ -39,14 +43,18 @@ public class CashClosedPayInventoryController {
     }
 
     @RequestMapping("selectCashClosedPayInventory")
-    public HashMap selectCashClosedPayInventory(){
-        List<CashClosedPayInventory> cashClosedPayInventoryList = cashClosedPayInventoryService.selectCashClosedPayInventory();
-        HashMap cashClosedPayInventoryMap = new HashMap();
-        cashClosedPayInventoryMap.put("count",10);
+    public Map<String,Object> selectCashClosedPayInventory(String page,String limit){
+        //调用Service层执行查询，接收返回结果集Map
+        Map<String, Object> map = cashClosedPayInventoryService.selectCashClosedPayInventory(limit,page);
+        List<CashClosedPayInventory> cashClosedPayInventoryList = (List<CashClosedPayInventory>) map.get("cashClosedPayInventoryList");
+        int count = (int) map.get("count");
+        //以layui要求存储响应数据格式
+        Map<String, Object> cashClosedPayInventoryMap = new HashMap<>();
         cashClosedPayInventoryMap.put("code",0);
         cashClosedPayInventoryMap.put("msg","");
+        cashClosedPayInventoryMap.put("count",count);
         cashClosedPayInventoryMap.put("data",cashClosedPayInventoryList);
-        System.out.println("已有"+cashClosedPayInventoryList.size()+"条数据");
+        //返回数据
         return cashClosedPayInventoryMap;
     }
 }
