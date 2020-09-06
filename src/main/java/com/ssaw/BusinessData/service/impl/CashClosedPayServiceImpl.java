759b309175503e126bfbb0b98728640b547c965e
Quekui <1049047882@qq.com>
2020-09-06 22:04:24 +0800

    阙魁2020/09/05
    BusinessDescription 实现了新增时的判断
    CashClosedPayController.java
    mapper
    CashClosedPayMapper.java
    service
    CashClosedPayService.java
    impl
    CashClosedPayServiceImpl.java
    
    resources
    CashClosedPayMapper.xml
    
    templates
    cashClosedPay.html

---
+++ src/main/java/com/ssaw/BusinessData/service/impl/CashClosedPayServiceImpl.java
@@ -0,0 +1,81 @@
+package com.ssaw.BusinessData.service.impl;
+
+
+import com.ssaw.BusinessData.entity.CashClosedPay;
+import com.ssaw.BusinessData.mapper.CashClosedPayMapper;
+import com.ssaw.BusinessData.service.CashClosedPayService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.*;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @ClassName CashClosePayServiceImpl
+ * @Description: TODO
+ * @Author 阙魁
+ * @Date create in 22:53 2020/9/1
+ * @Version 1.0
+ **/
+@Service
+public class CashClosedPayServiceImpl implements CashClosedPayService {
+    @Resource
+    CashClosedPayMapper cashClosedPayMapper;
+
+    @Override
+    public int insertCashClosedPay(CashClosedPay cashClosedPay) {
+        return cashClosedPayMapper.insertCashClosedPay(cashClosedPay);
+    }
+
+    @Override
+    public int deleteCashClosedPay(String cashClosedPayId) {
+        return cashClosedPayMapper.deleteCashClosedPay(cashClosedPayId);
+    }
+
+    @Override
+    public int updateCashClosedPay(CashClosedPay cashClosedPay) {
+        return cashClosedPayMapper.updateCashClosedPay(cashClosedPay);
+    }
+
+    @Override
+    public Map<String,Object> selectCashClosedPay(String pageSize, String page) {
+        //创建一个结果集用于接受存储过程的返回结果
+        Map<String,Object> resultMap = new HashMap<>();
+        //存储过程所需条件为p_tableName/p_condition/p_page/p_pageSize/p_count/p_cursor
+        //定义一个分页条数变量
+        int v_pageSize=0;
+        //判断传入的pageSize是否为null/空
+        if (pageSize!=null && pageSize.equals("")){
+            //通过Integer包装类将String类型转换成int基础数据类型
+            v_pageSize= Integer.parseInt(pageSize);
+        }
+        //定义一个分页页码变量
+        int v_page=0;
+        //判断传入的page是否为null/空
+        if(page!=null&&!page.equals("")) {
+            //通过Integer包装类将String类型转换成int基础数据类型
+            v_page=Integer.parseInt(page);
+        }
+//创建一个结果集用于接收数据库存储过程所需条件
+        Map<String,Object> map = new HashMap<>();
+        String sqlSelect="(select * from cashClosedPay c join fund f on f.fundId=c.fundId join securities s on s.securitiesId=c.securitiesId join account a on a.accountId=c.accountId ) ";
+        map.put("p_tableName",sqlSelect);
+        map.put("p_condition","");
+        map.put("p_pageSize",v_pageSize);
+        map.put("p_page",v_page);
+        map.put("p_count",0);
+        map.put("p_cursor",null);
+
+        //调用Mapper执行查询
+        cashClosedPayMapper.selectCashClosedPay(map);
+        //接收返回数据
+        List<CashClosedPay> cashClosedPays = (List<CashClosedPay>) map.get("p_cursor");
+        //接收返回总条数
+        int count = (int) map.get("p_count");
+        //将结果放入结果集Map
+        resultMap.put("cashClosedPays",cashClosedPays);
+        resultMap.put("count",count);
+        return resultMap;
+    }
+}
