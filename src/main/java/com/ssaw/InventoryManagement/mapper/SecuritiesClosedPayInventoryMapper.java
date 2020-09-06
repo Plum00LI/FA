f6d0ad9e893171c9e047ca34731b38b8d4ffc228
sss <34123412@234>
2020-09-06 21:37:38 +0800

    佘语桐20200906 新增 SecuritiesClosedPayInventoryController.java SecuritiesClosedPayInventory.java SecuritiesClosedPayInventoryMapper.java SecuritiesClosedPayInventoryServiceImpl.java SecuritiesClosedPayInventoryService.java securitiesClosedPayInventoryMapper.xml 修改 cashInventory.html taInventory.html

---
+++ src/main/java/com/ssaw/InventoryManagement/mapper/SecuritiesClosedPayInventoryMapper.java
@@ -0,0 +1,45 @@
+package com.ssaw.InventoryManagement.mapper;
+
+import com.ssaw.InventoryManagement.entity.SecuritiesClosedPayInventory;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * ClassName:    SecuritiesClosedPayInventoryMapper
+ * Package:    com.ssaw.InventoryManagement.mapper
+ * Description:
+ * Version:
+ * Datetime:    2020/9/6   21:02
+ * Author:   SYT
+ */
+@Mapper
+public interface SecuritiesClosedPayInventoryMapper {
+    /**
+     * 按条件分页查询所有证券应收应付库存
+     * @param map
+     */
+    public void selectSecuritiesClosedPayInventory(Map map);
+
+    /**
+     * 增加证券应收应付库存
+     * @param securitiesClosedPayInventory
+     * @return
+     */
+    public int insertSecuritiesClosedPayInventory(SecuritiesClosedPayInventory securitiesClosedPayInventory);
+
+    /**
+     * 修改证券应收应付库存信息
+     * @param securitiesClosedPayInventory
+     * @return
+     */
+    public int updateSecuritiesClosedPayInventory(SecuritiesClosedPayInventory securitiesClosedPayInventory);
+
+    /**
+     * 批量删除
+     * @param securitiesClosedPayInventoryId
+     * @return
+     */
+    public int deleteSecuritiesClosedPayInventory(List securitiesClosedPayInventoryId);
+}
