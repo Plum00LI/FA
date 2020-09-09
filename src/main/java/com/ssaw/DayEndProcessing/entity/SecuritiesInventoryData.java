e1683040329e07dd6c3680c96b0657482582e695
sss <34123412@234>
2020-09-09 20:40:02 +0800

    佘语桐20200909 修改 InventoryStatisticsController.java SecuritiesInventoryData.java InventoryStatisticsServiceImpl.java inventoryStatisticsMapper.xml inventoryStatistics.js 新增 InventoryStatisticsMapper.java InventoryStatisticsService.java cashInventory.js securitiesClosedPaylnventory.js taInventory.js cashInventory.html inventoryStatistics.html

---
+++ src/main/java/com/ssaw/DayEndProcessing/entity/SecuritiesInventoryData.java
@@ -0,0 +1,13 @@
+package com.ssaw.DayEndProcessing.entity;
+
+/**
+ * ClassName:    SecuritiesInventoryData
+ * Package:    com.ssaw.DayEndProcessing.entity
+ * Description:
+ * Version:
+ * Datetime:    2020/9/9   19:31
+ * Author:   SYT
+ */
+public class SecuritiesInventoryData {
+
+}
---
+++ src/main/java/com/ssaw/DayEndProcessing/service/impl/InventoryStatisticsServiceImpl.java
@@ -0,0 +1,13 @@
+package com.ssaw.DayEndProcessing.service.impl;
+
+/**
+ * ClassName:    InventoryStatisticsServiceImpl
+ * Package:    com.ssaw.DayEndProcessing.service.impl
+ * Description:
+ * Version:
+ * Datetime:    2020/9/9   17:18
+ * Author:   SYT
+ */
+public class InventoryStatisticsServiceImpl {
+
+}
