a4bb1953dda042f073c34caf49e3acead98b1412
sunH_ <779874728@qq.com>
2020-09-09 20:43:00 +0800

    sunH 2020/09/09
    EquityDisposeController.java
    EquityDispose.java
    EquityDisposeMapper.java
    EquityDisposeServiceImpl.java
    EquityDisposeService.java
    
    resources:
    EquityDisposeMapper.xml
    equityData.js
    equityData.html
    equityDispose.html

---
+++ src/main/java/com/ssaw/BusinessProcessing/controller/EquityDisposeController.java
@@ -0,0 +1,41 @@
+package com.ssaw.BusinessProcessing.controller;
+
+import com.ssaw.BusinessData.entity.EquityData;
+import com.ssaw.BusinessProcessing.entity.EquityDispose;
+import com.ssaw.BusinessProcessing.service.EquityDisposeService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @program:TescComment
+ * @Description:实体类
+ * @author:孙浩
+ * @create:2020-09-01
+ */
+@RestController
+public class EquityDisposeController {
+    @Resource
+    EquityDisposeService equityDisposeService;
+
+    @RequestMapping("selectEquityDispose")
+    public Map<String,Object> selectEquityDispose(String page,String limit){
+        //调用Service层执行查询，接收返回结果集Map
+        Map<String, Object> map = equityDisposeService.selectEquityDispose(limit,page);
+        List<EquityDispose> equityDisposeList = (List<EquityDispose>) map.get("equityDisposeList");
+        int count = (int) map.get("count");
+        //以layui要求存储响应数据格式
+        Map<String, Object> equityDisposeMap = new HashMap<>();
+        equityDisposeMap.put("code",0);
+        equityDisposeMap.put("msg","");
+        equityDisposeMap.put("count",count);
+        equityDisposeMap.put("data",equityDisposeList);
+        //返回数据
+        System.out.println(equityDisposeList);
+        return equityDisposeMap;
+    }
+}
