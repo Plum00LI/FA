594c9ed8328219d66e71fb99cc9299ba67c9a953
plum <15120022843@126.com>
2020-09-09 08:48:26 +0800

    plum 2020/09/09 version1.8
    FAApplication.java

--- src/main/java/com/ssaw/FAApplication.java
+++ src/main/java/com/ssaw/FAApplication.java
@@ -3,6 +3,9 @@ package com.ssaw;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 主引导
+ */
 @SpringBootApplication
 public class FAApplication {
     public static void main(String[] args) {
