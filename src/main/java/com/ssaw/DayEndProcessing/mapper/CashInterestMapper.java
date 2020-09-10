0435045504c3bf2a2fa4ca4b83c50092d5e24342
plum <15120022843@126.com>
2020-09-10 08:48:57 +0800

    plum 2020/09/10 version1.10
    程序异常文件修复

--- src/main/java/com/ssaw/DayEndProcessing/mapper/CashInterestMapper.java
+++ src/main/java/com/ssaw/DayEndProcessing/mapper/CashInterestMapper.java
@@ -1,8 +1,8 @@
 package com.ssaw.DayEndProcessing.mapper;
 
-//import com.ssaw.DayEndProcessing.entity.BondInterest;
-//import com.ssaw.DayEndProcessing.entity.CashInterest;
-//import com.ssaw.DayEndProcessing.entity.TwoFeeInterest;
+import com.ssaw.DayEndProcessing.entity.BondInterest;
+import com.ssaw.DayEndProcessing.entity.CashInterest;
+import com.ssaw.DayEndProcessing.entity.TwoFeeInterest;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
@@ -17,9 +17,9 @@ public interface CashInterestMapper {
      * 按条件查询 分页查询
      * @return 按业务时间查询的集合
      */
-//    public List<CashInterest> selectCashInterest(String dateTime);
-    //查询债券计息
-//    public List<BondInterest> selectBondInterest(String dateTime);
-    //查询两费
-//    public List<TwoFeeInterest> selectTwoFeeInterest(String dateTime);
+    public List<CashInterest> selectCashInterest(String dateTime);
+//    查询债券计息
+    public List<BondInterest> selectBondInterest(String dateTime);
+//    查询两费
+    public List<TwoFeeInterest> selectTwoFeeInterest(String dateTime);
 }
--- src/main/java/com/ssaw/DayEndProcessing/service/CashInterestService.java
+++ src/main/java/com/ssaw/DayEndProcessing/service/CashInterestService.java
@@ -1,9 +1,9 @@
 package com.ssaw.DayEndProcessing.service;
 
 import com.ssaw.BusinessDescription.entity.Stock;
-//import com.ssaw.DayEndProcessing.entity.BondInterest;
-//import com.ssaw.DayEndProcessing.entity.CashInterest;
-//import com.ssaw.DayEndProcessing.entity.TwoFeeInterest;
+import com.ssaw.DayEndProcessing.entity.BondInterest;
+import com.ssaw.DayEndProcessing.entity.CashInterest;
+import com.ssaw.DayEndProcessing.entity.TwoFeeInterest;
 
 import java.util.List;
 import java.util.Map;
@@ -17,9 +17,9 @@ public interface CashInterestService {
      * 分页查询 调用存储过程
      * @return 条件查询的集合
      */
-//    public List<CashInterest> selectCashInterest(String dateTime);
-//    //查询债券计息
-//    public List<BondInterest> selectBondInterest(String dateTime);
-//    //查询两费
-//    public List<TwoFeeInterest> selectTwoFeeInterest(String dateTime);
+    public List<CashInterest> selectCashInterest(String dateTime);
+    //查询债券计息
+    public List<BondInterest> selectBondInterest(String dateTime);
+    //查询两费
+    public List<TwoFeeInterest> selectTwoFeeInterest(String dateTime);
 }
