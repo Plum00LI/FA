5c79e5450ffbf3a82554893f1c2919e0cc98a4eb
傅赛赢 <1176040487@qq.com>
2020-09-14 19:13:23 +0800

    2020/09/14 RevenueProvisionController.java BondInterest.java RevenueProvision.java TwoFees.java RevenueProvisionMapper.java RevenueProvisionServiceImpl.java RevenueProvisionService.java RevenueProvisionMapper.xml revenueProvision.html

--- src/main/java/com/ssaw/DayEndProcessing/entity/BondInterest.java
+++ src/main/java/com/ssaw/DayEndProcessing/entity/BondInterest.java
@@ -1,43 +1,193 @@
 package com.ssaw.DayEndProcessing.entity;
 
-/**
- * 债券计息实体类
- */
 public class BondInterest {
-    /* securitiesInventory and bond
-    securitiesInventoryId Varchar2(50)                                not null,          *Varchar2(50)	PK 证券库存ID*
-    dateTime varchar2(10)                                not null,                      *证券库存日期*
-    securitiesId Varchar2(50)                                not null,        * 证券信息表ID，引用证券编号securitiesId，证券名securitiesName*
-    fundId Varchar2(50)                                not null,                *基金表Id 印入基金代码*
-    securityPeriodFlag Number(16,2)                                not null,         * 是否从其他系统导入的期初数据  0：不是  1：是*
-    securitiesNum Number(16,2)                                not null,   *计算总金额条件 证券的数量*
-    price Number(16,2)                                not null,                      *计算总金额条件 单位成本*
-    total Number(16,2)                                not null,                      *证券数量*单位成本*
-    securitiesInventoryDesc Varchar(50)                                                                    *备注*
 
-    securitiesId            varchar2(50)        primary key         not null ,      -- 证券编号
-    bondName                varchar2(50)                            not null ,      -- 债券名称
-    drawStartDate           varchar2(12)                            not null ,      -- 计息起始日期
-    drawEndDate             varchar2(12)                            not null ,      -- 计息结束日期
-    bondType                number(1)                               not null ,      -- 债券类型  1=银行间  0=非银行间
-    parRate                 number(16,4)                            not null ,      -- 票面利率
-    bondRate                number(16,4)                            not null ,      -- 债券利息
-    bondRateAmount          varchar2(50)                            not null ,      -- 票面金额
-    payInterestNum          number(1)                               not null ,      -- 付息次数  1=1年一次  2=1年俩次 3=1年四次
-    bondDesc                varchar2(12)                                            -- 备注
-    */
-    private String bondName;//债券名称
-    private String securitiesId;//证券编号
-    private int bondType;//债券类型  1=银行间  0=非银行间
-    private String bondRateAmount;//票面金额
-    private String dateTime;//证券库存日期
-    private String drawStartDate;//计息起始日期
-    private double bondRate;//债券利息
-    private double securitiesNum;//证券的数量
-    private double parRate;//票面利率
-    private int payInterestNum;//付息次数
+    private String securitiesId;  //证券编号
+    private String bondName;  //证券名称
+    private String drawStartDate;//计息起始日
+    private  String drawEndDate; //计息结束日
+    private int bondType; //债券类型
+    private double parRate; //票面利率
+    private double bondRate; //债券利息
+    private double bondRateAmount;//票面金额
+    private  int payInterestNum; //付息次数Id
+    private  String payInterest ; //付息次数
+    private String fundId; //基金Id
+    private String accountId; //账户Id
+    private int securityPeriodFlag; //是否导入初期数据
+    private int securitiesNum; //债券数量
+    private String dateTime; //统计日期
+    private double interest;
 
     public BondInterest() {
     }
 
+    public String getSecuritiesId() {
+        return securitiesId;
+    }
+
+    public void setSecuritiesId(String securitiesId) {
+        this.securitiesId = securitiesId;
+    }
+
+    public String getBondName() {
+        return bondName;
+    }
+
+    public void setBondName(String bondName) {
+        this.bondName = bondName;
+    }
+
+    public String getDrawStartDate() {
+        return drawStartDate;
+    }
+
+    public void setDrawStartDate(String drawStartDate) {
+        this.drawStartDate = drawStartDate;
+    }
+
+    public String getDrawEndDate() {
+        return drawEndDate;
+    }
+
+    public void setDrawEndDate(String drawEndDate) {
+        this.drawEndDate = drawEndDate;
+    }
+
+    public int getBondType() {
+        return bondType;
+    }
+
+    public void setBondType(int bondType) {
+        this.bondType = bondType;
+    }
+
+    public double getParRate() {
+        return parRate;
+    }
+
+    public void setParRate(double parRate) {
+        this.parRate = parRate;
+    }
+
+    public double getBondRate() {
+        return bondRate;
+    }
+
+    public void setBondRate(double bondRate) {
+        this.bondRate = bondRate;
+    }
+
+    public double getBondRateAmount() {
+        return bondRateAmount;
+    }
+
+    public void setBondRateAmount(double bondRateAmount) {
+        this.bondRateAmount = bondRateAmount;
+    }
+
+    public int getPayInterestNum() {
+        return payInterestNum;
+    }
+
+    public void setPayInterestNum(int payInterestNum) {
+        this.payInterestNum = payInterestNum;
+    }
+
+    public String getPayInterest() {
+        return payInterest;
+    }
+
+    public void setPayInterest(String payInterest) {
+        this.payInterest = payInterest;
+    }
+
+    public String getFundId() {
+        return fundId;
+    }
+
+    public void setFundId(String fundId) {
+        this.fundId = fundId;
+    }
+
+    public String getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(String accountId) {
+        this.accountId = accountId;
+    }
+
+    public int getSecurityPeriodFlag() {
+        return securityPeriodFlag;
+    }
+
+    public void setSecurityPeriodFlag(int securityPeriodFlag) {
+        this.securityPeriodFlag = securityPeriodFlag;
+    }
+
+    public int getSecuritiesNum() {
+        return securitiesNum;
+    }
+
+    public void setSecuritiesNum(int securitiesNum) {
+        this.securitiesNum = securitiesNum;
+    }
+
+    public String getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(String dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public double getInterest() {
+        return interest;
+    }
+
+    public void setInterest(double interest) {
+        this.interest = interest;
+    }
+
+    public BondInterest(String securitiesId, String bondName, String drawStartDate, String drawEndDate, int bondType, double parRate, double bondRate, double bondRateAmount, int payInterestNum, String payInterest, String fundId, String accountId, int securityPeriodFlag, int securitiesNum, String dateTime, double interest) {
+        this.securitiesId = securitiesId;
+        this.bondName = bondName;
+        this.drawStartDate = drawStartDate;
+        this.drawEndDate = drawEndDate;
+        this.bondType = bondType;
+        this.parRate = parRate;
+        this.bondRate = bondRate;
+        this.bondRateAmount = bondRateAmount;
+        this.payInterestNum = payInterestNum;
+        this.payInterest = payInterest;
+        this.fundId = fundId;
+        this.accountId = accountId;
+        this.securityPeriodFlag = securityPeriodFlag;
+        this.securitiesNum = securitiesNum;
+        this.dateTime = dateTime;
+        this.interest = interest;
+    }
+
+    @Override
+    public String toString() {
+        return "BondInterest{" +
+                "securitiesId='" + securitiesId + '\'' +
+                ", bondName='" + bondName + '\'' +
+                ", drawStartDate='" + drawStartDate + '\'' +
+                ", drawEndDate='" + drawEndDate + '\'' +
+                ", bondType=" + bondType +
+                ", parRate=" + parRate +
+                ", bondRate=" + bondRate +
+                ", bondRateAmount=" + bondRateAmount +
+                ", payInterestNum=" + payInterestNum +
+                ", payInterest='" + payInterest + '\'' +
+                ", fundId='" + fundId + '\'' +
+                ", accountId='" + accountId + '\'' +
+                ", securityPeriodFlag=" + securityPeriodFlag +
+                ", securitiesNum=" + securitiesNum +
+                ", dateTime='" + dateTime + '\'' +
+                ", interest=" + interest +
+                '}';
+    }
 }
